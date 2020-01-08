26bcb6d2265d954e9151610269a5ca9f460896cd
student <stu@ravtech.local>
2020-01-08 14:26:02 +0200

    '1'

--- app/src/main/java/com/example/homworkmoovy/MainActivity.java
+++ app/src/main/java/com/example/homworkmoovy/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.homworkmoovy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void moovToPage(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=dFZHMHjdB8w"));
+        startActivity(intent);
     }
 }
